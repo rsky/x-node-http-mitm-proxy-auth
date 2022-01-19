4295917a9f936f58744fb65b2f96eba2e8d9e644
Ryusuke Sekiyama <rsky@ra-bo.com>
2022-01-19 17:07:23 +0900

    tweak

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const Proxy = require('http-mitm-proxy');
 const proxy = Proxy();
 
+// settings
 const port = 8081;
 const username = 'mitm';
 const password = 'proxy';
+
+// constants
 const credentials = Buffer.from(`${username}:${password}`).toString('base64');
+const expectedProxyAuthorization = `Basic ${credentials}`;
+const proxyAuthenticate = 'Basic realm="MITM Proxy"';
 
 proxy.onError((ctx, err) => {
   console.error('proxy error:', err);
 });
 
 proxy.onRequest((ctx, callback) => {
-  console.log(`${ctx.clientToProxyRequest.method} http://${ctx.clientToProxyRequest.headers.host}${ctx.clientToProxyRequest.url}`);
+  console.log(`${ctx.clientToProxyRequest.method} ${ctx.isSSL ? 'https' : 'http'}://${ctx.clientToProxyRequest.headers.host}${ctx.clientToProxyRequest.url}`);
 
-  if (ctx.clientToProxyRequest.headers['proxy-authorization'] === `Basic ${credentials}`) {
+  if (ctx.clientToProxyRequest.headers['proxy-authorization'] === expectedProxyAuthorization) {
     return callback();
   }
 
   console.log('proxy authentication required');
   ctx.proxyToClientResponse.statusCode = 407;
   ctx.proxyToClientResponse.statusMessage = 'Proxy Authentication Required';
-  ctx.proxyToClientResponse.setHeader('proxy-authenticate', 'Basic realm="MITM Proxy"');
+  ctx.proxyToClientResponse.setHeader('proxy-authenticate', proxyAuthenticate);
   ctx.proxyToClientResponse.end();
   // no callback() so proxy request is not sent to the server
 });
